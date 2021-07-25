00ab59da590802f06cfb0995f2274ac08ec68621
CrazyQii <858590598@qq.com>
2021-07-26 00:35:34 +0800

    update

--- flaskr/settings.py
+++ flaskr/settings.py
@@ -4,7 +4,7 @@
 class Config(object):
     """ 基础配置文件 """
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:hsy98106@127.0.0.1:3306/wenjuan'  # 数据库配置连接
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:*****@127.0.0.1:3306/wenjuan'  # 数据库配置连接
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'exchange_system'  # flaks 密钥
