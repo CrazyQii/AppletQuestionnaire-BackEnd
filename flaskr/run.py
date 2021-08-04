2c1bccf93d0aa36b9eee98cc42e5ac474e71479d
CrazyQii <858590598@qq.com>
2021-08-04 09:00:30 +0800

    update

--- flaskr/run.py
+++ flaskr/run.py
@@ -11,7 +11,8 @@ def create_app():
     # 设置跨域请求
     # CORS(app, supports_credentials=True)
 
-    app.config.from_object(settings.ProductionConfig)  # 加载配置
+    app.config.from_object(settings.DevelopmentConfig)  # 加载开发配置
+    # app.config.from_object(settings.ProductionConfig)  # 加载生产配置
 
     models.init_app(app)
     routes.init_app(app)
@@ -26,4 +27,7 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=4443, ssl_context=('5500590_www.crazyqiqi.top.crt', '5500590_www.crazyqiqi.top.key'))
+    # 远程服务器运行
+    # app.run(host='0.0.0.0', port=4443, ssl_context=('5500590_www.crazyqiqi.top.crt', '5500590_www.crazyqiqi.top.key'))
+    # 本地服务器运行
+    app.run(host='127.0.0.1', port=5000)
--- flaskr/settings.py
+++ flaskr/settings.py
@@ -4,17 +4,19 @@
 class Config(object):
     """ 基础配置文件 """
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@116.62.27.4:3306/wenjuan'  # 数据库配置连接
+    
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'exchange_system'  # flaks 密钥
     
 class DevelopmentConfig(Config):
     """ 开发环境 """
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:hsy98106@127.0.0.1:3306/wenjuan'  # 远程数据库配置连接
     ENV = 'development'    
     DEBUG = True
     
 class ProductionConfig(Config):
     """ 生产环境 """
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@116.62.27.4:3306/wenjuan'  # 远程数据库配置连接
     ENV = 'production'   
     DEBUG = False 
